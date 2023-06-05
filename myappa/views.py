5cce589339fb05b2bc493933260aac7f05acff5b
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2023-06-05 15:13:44 -0700

    first version

---
+++ myappa/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# def index(request) :
+#     context = {
+#         'name' : 'David',
+#         'age' : 19,
+#         'goal' : 'software-engineer'
+#     }
+#     return render(request, 'index.html', context)
+
+
+
+def counter(request) : 
+    words = request.GET['words']
+    amount_of_words = len(words.split())
+    return render(request, 'counter.html', {'amount' : amount_of_words})
